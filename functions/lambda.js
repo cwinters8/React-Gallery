26a69e775d749fcd3b88b869811da03f4ae1f2c5
cwinters8 <clwinters8@gmail.com>
2019-02-23 18:09:57 -0800

    lambda returning good data!

--- functions/lambda.js
+++ functions/lambda.js
@@ -1,8 +1,19 @@
+const https = require('https');
+
 export function handler(event, context, callback) {
   const apikey = process.env.APIKEY;
   const query = event.queryStringParameters.q;
-  callback(null, {
-    statusCode: 200,
-    body: JSON.stringify({ data: query }),
+  const url = `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apikey}&tags=${query}&sort=interestingness-desc&per_page=24&format=json&nojsoncallback=1`;
+  const request = https.get(url, res => {
+    let body = '';
+    res.on('data', data => {
+      body += data;
+    });
+    res.on('end', () => {
+      callback(null, {
+        statusCode: 200,
+        body: JSON.stringify({data: body}),
+      });
+    })
   });
 }
